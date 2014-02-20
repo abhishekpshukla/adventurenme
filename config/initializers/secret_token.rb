b7df3d3e068f708273162f34a4ced79afd8d084a
Abhishek Shukla <abhishekpshukla@gmail.com>
2014-02-20 23:36:28 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adventurenme::Application.config.secret_key_base = '6b0c2c81afc2a37a7b8d00757fe0b305e2ee78d5e2d9fbcad772dae3922eb7ca152f2af4df4b397145410c76535bb5b7e1c4e1caf9208ccc48216bfa4d0e0120'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adventurenme::Application.config.session_store :cookie_store, key: '_adventurenme_session'
